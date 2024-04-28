a0448bd6e26b0b24d694a182604c317f157e7a72
shiki-01 <0110shiki@gmail.com>
2024-04-28 11:10:44 +0900

    Update microcms.ts

--- my-svelte-app/src/lib/services/microcms.ts
+++ my-svelte-app/src/lib/services/microcms.ts
@@ -1,6 +1,4 @@
 import { createClient, type MicroCMSQueries } from "microcms-js-sdk";
-import blogsDetail from "$lib/assets/mocks/blogs-detail.json";
-import blogsList from "$lib/assets/mocks/blogs-list.json";
 import type { EndPoints } from "$lib/types/microcms";
 
 const MICROCMS_SERVICE_DOMAIN = import.meta.env.VITE_MICROCMS_SERVICE_DOMAIN;
@@ -15,14 +13,6 @@ export async function getContentList<T extends keyof EndPoints["gets"]>(
   key: T,
   queries: MicroCMSQueries = {},
 ): Promise<EndPoints["gets"][T]> {
-  if (process.env.NODE_ENV === "development") {
-    switch (key) {
-      case "blogs":
-        return blogsList as any;
-      default:
-        throw new Error("Invalid key");
-    }
-  }
 
   return microcms.get({
     endpoint: key,
@@ -35,14 +25,6 @@ export async function getContentDetail<T extends keyof EndPoints["get"]>(
   id: string,
   queries: MicroCMSQueries = {},
 ): Promise<EndPoints["get"][T]> {
-  if (process.env.NODE_ENV === "development") {
-    switch (key) {
-      case "blogs":
-        return blogsDetail as any;
-      default:
-        throw new Error("Invalid key");
-    }
-  }
   return microcms.getListDetail<EndPoints["get"][T]>({
     endpoint: key,
     contentId: id,
