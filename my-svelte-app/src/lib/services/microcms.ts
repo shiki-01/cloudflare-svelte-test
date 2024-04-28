cbf6fe72577007ad7c643f85b9724a8022ffeb0d
shiki-01 <0110shiki@gmail.com>
2024-04-28 10:01:32 +0900

    ♻️ update 環境変数当たり

--- my-svelte-app/src/lib/services/microcms.ts
+++ my-svelte-app/src/lib/services/microcms.ts
@@ -1,12 +1,12 @@
 import { createClient, type MicroCMSQueries } from "microcms-js-sdk";
-import { MICROCMS_SERVICE_DOMAIN, MICROCMS_API_KEY } from "$env/static/private";
+import { VITE_MICROCMS_SERVICE_DOMAIN, VITE_MICROCMS_API_KEY } from "$env/static/private";
 import blogsDetail from "$lib/assets/mocks/blogs-detail.json";
 import blogsList from "$lib/assets/mocks/blogs-list.json";
 import type { EndPoints } from "$lib/types/microcms";
 
 export const microcms = createClient({
-  serviceDomain: MICROCMS_SERVICE_DOMAIN,
-  apiKey: MICROCMS_API_KEY,
+  serviceDomain: import.meta.env.VITE_MICROCMS_SERVICE_DOMAIN,
+  apiKey: import.meta.env.MICROCMS_API_KEY,
 });
 
 export async function getContentList<T extends keyof EndPoints["gets"]>(
