250d6071f2acf65c19e1aa2006a7c229a2cc4247
shiki-01 <0110shiki@gmail.com>
2024-04-28 11:17:43 +0900

    なくしてみた２

--- my-svelte-app/src/lib/services/microcms.ts
+++ my-svelte-app/src/lib/services/microcms.ts
@@ -15,15 +15,6 @@ export async function getContentList<T extends keyof EndPoints["gets"]>(
   key: T,
   queries: MicroCMSQueries = {},
 ): Promise<EndPoints["gets"][T]> {
-  if (process.env.NODE_ENV === "development") {
-    switch (key) {
-      case "blogs":
-        return blogsList as any;
-      default:
-        throw new Error("Invalid key");
-    }
-  }
-
   return microcms.get({
     endpoint: key,
     queries,
@@ -35,14 +26,6 @@ export async function getContentDetail<T extends keyof EndPoints["get"]>(
   id: string,
   queries: MicroCMSQueries = {},
 ): Promise<EndPoints["get"][T]> {
-  if (process.env.NODE_ENV === "development") {
-    switch (key) {
-      case "blogs":
-        return blogsDetail as any;
-      default:
-        throw new Error("Invalid key");
-    }
-  }
   return microcms.getListDetail<EndPoints["get"][T]>({
     endpoint: key,
     contentId: id,
